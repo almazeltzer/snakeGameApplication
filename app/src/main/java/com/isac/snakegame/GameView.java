0eb1af4b6e0be823de1a9213d7517a88e728a230
alma <s830308@guttman.ort.org.il>
2024-03-29 13:23:21 +0300

    design

--- app/src/main/java/com/isac/snakegame/GameView.java
+++ app/src/main/java/com/isac/snakegame/GameView.java
@@ -1,13 +1,20 @@
 package com.isac.snakegame;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class GameView extends View {
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    @Override
+    public void draw(@NonNull Canvas canvas) {
+        super.draw(canvas);
     }
 }
---
+++ app/src/main/java/com/isac/snakegame/Grass.java
@@ -0,0 +1,9 @@
+package com.isac.snakegame;
+
+import android.graphics.Bitmap;
+
+public class Grass {
+    private Bitmap bm;
+    private int x,y,width,height;
+
+}
