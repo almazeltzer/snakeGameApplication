b6c214614bf0b79b71dd8636e763f6a22a08ad32
dhr <s830308@guttman.ort.org.il>
2024-05-03 12:33:18 +0300

    ghto

--- app/src/main/java/com/isac/snakegame/GameView.java
+++ app/src/main/java/com/isac/snakegame/GameView.java
@@ -1,6 +1,8 @@
 package com.isac.snakegame;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
@@ -9,8 +11,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class GameView extends View {
+    private Bitmap bmGrass1,bmGrass2;
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        bmGrass1= BitmapFactory.decodeResource(this.getResources(),R.drawable.grass);
+
     }
 
     @Override
