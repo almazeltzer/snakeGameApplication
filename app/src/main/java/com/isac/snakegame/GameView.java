1c95e340a9b577c686404e62b8fe6e55b3a3d4d6
Alma <s830308@guttman.ort.org.il>
2024-05-24 16:14:47 +0300

    Created classes:Snake&& PartsSnake (some edits in GameView)

--- app/src/main/java/com/isac/snakegame/GameView.java
+++ app/src/main/java/com/isac/snakegame/GameView.java
@@ -13,19 +13,22 @@ import androidx.annotation.Nullable;
 import java.util.ArrayList;
 
 public class GameView extends View {
-    private Bitmap bmGrass1,bmGrass2;
+    private Bitmap bmGrass1,bmGrass2,bmSnake;
     public static int sizeOfMap= 75*Constants.SCREEN_WIDTH/1080;
     private int h= 21,w=12;
     private ArrayList<Grass> arrGrass=new ArrayList<>();
+    private Snake snake;
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         bmGrass1= BitmapFactory.decodeResource(this.getResources(),R.drawable.grass);
         bmGrass1= Bitmap.createScaledBitmap(bmGrass1,sizeOfMap,sizeOfMap,true);
         bmGrass2= BitmapFactory.decodeResource(this.getResources(),R.drawable.grass03);
         bmGrass2= Bitmap.createScaledBitmap(bmGrass2,sizeOfMap,sizeOfMap,true);
+        bmSnake= BitmapFactory.decodeResource(this.getResources(),R.drawable.snake1);
+        bmSnake= Bitmap.createScaledBitmap(bmSnake,14*sizeOfMap,sizeOfMap,true);
         for(int i=0;i<h;i++)
         {
-            for(int j=o;j<w;i++)
+            for(int j=0;j<w;i++)
             {
                 if((i+j)%2==0)
                 {
@@ -37,17 +40,18 @@ public class GameView extends View {
                 }
             }
         }
-
+        snake=new Snake(bmSnake,arrGrass.get(126).getX(),arrGrass.get(126).getY(),4);
     }
 
     @Override
-    public void draw(@NonNull Canvas canvas) {
+    public void draw( Canvas canvas) {
         super.draw(canvas);
         canvas.drawColor( 0XFF1A6100);
         for(int i=0;i< arrGrass.size();i++)
         {
             canvas.drawBitmap(arrGrass.get(i).getBm(),arrGrass.get(i).getX(),arrGrass.get(i).getY(),null );
         }
+        snake.draw(canvas);
     }
 
 }
